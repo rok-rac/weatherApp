31ff5b766d46e149d8f5ccc836810c1a7a6b87dc
rokas_rac <rokas@tandemum.lt>
2021-09-20 17:38:08 +0300

    data setup

--- weatherApp/database/seeders/DatabaseSeeder.php
+++ weatherApp/database/seeders/DatabaseSeeder.php
@@ -13,6 +13,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+         \App\Models\Product::factory(100)->create();
+         $seeder = new \Database\Seeders\ConditionsSeeder();
+         $seeder->run();     
+         \App\Models\ProductCondition::factory(50)->create();
     }
 }
